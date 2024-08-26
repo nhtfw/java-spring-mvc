0822326d8784ce29381005895fc3a4f78552ed80
nhtfw <huuthangfw@gmail.com>
2024-08-26 14:11:22 +0700

    26/8 chieu

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -8,7 +8,17 @@ public class HelloController {
 
     @GetMapping("/")
     public String index() {
-        return "Hello World from NHT";
+        return "Hello World";
+    }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "Only users can access this page";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Only admins can access this page";
     }
 
 }
--- src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
+++ src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// include >< exclude
+// @SpringBootApplication(exclude =
+// org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
+
 public class LaptopshopApplication {
 
 	public static void main(String[] args) {
